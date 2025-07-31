c421272a755e343cc52429fb41a139dddae4d4d3
Ethanzzz21 <ethan.zaoui@student.scots.college>
2025-07-31 19:55:32 +1000

    Adding gravity and finishing set up of game window

--- ElementalEscapeGame.py
+++ ElementalEscapeGame.py
@@ -2,33 +2,60 @@ import pygame as py
 
 py.init() # Initialize Pygame Modules
 
+# Constants
+GREEN = (0, 128, 0)
+
+
 # Setting Up Window
 WIDTH, HEIGHT = 800, 600
+
+GRAVITY = 0.5
+GROUND_HEIGHT = 525
+
 nameOfGame = "Roguelike Shapeshifter TEST"
 
 py.display.set_mode((WIDTH, HEIGHT))
 py.display.set_caption(nameOfGame)
 
-py.draw.rect(py.display.get_surface(), (100, 100, 100), (0, 550, 800, 50))
-
-
-
+clock = py.time.Clock()
 
 startingPosition = (400, 300)
 
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, width=50, height=50):
         self.name = name
         self.position = startingPosition
+        self.width = width
+        self.height = height
 
-    def move(self, x, y):
+        self.velocity_y = 0
+
+
+
+
+        print(f"Player {self.name} created at position {self.position}")
+
+    def updateGravity(self):
+        # Apply gravity
+        self.velocity_y += GRAVITY
+        x, y = self.position
+        y += self.velocity_y
+
+        # Check for ground collision
+        if y >= GROUND_HEIGHT:
+            y = GROUND_HEIGHT
+            self.velocity_y = 0  # Reset vertical velocity on ground contact
+        
         self.position = (x, y)
 
-    def draw(self, ):
-        py.draw.rect(screen, color, (self.x, self.y, self.width, self.height))
+    def draw(self):
+        x, y = self.position
+        py.draw.rect(py.display.get_surface(), GREEN, ((x - (self.width/2)), (y - (self.height/2)), self.width, self.height))
+    
 
 
-Player("Player1")
+mainCharacter = Player("Player1")
+mainCharacter.draw()
 
 class Room():
     def __init__(self, layout):
@@ -43,12 +70,27 @@ class Room():
 running = True
 while running:
 
+
+    # Prepare the screen for next frame
+    py.display.get_surface().fill((0, 0, 0))  # Clear the screen
+    clock.tick(60) # Limit the frame rate to 60 FPS
+
+    # Draw the environment/room -- TEMPORARY
+    py.draw.rect(py.display.get_surface(), (100, 100, 100), (0, 550, 800, 50))
+
+
+
     # Respond to player input
     for event in py.event.get():
         # Quit the game
         if event.type == py.QUIT:
             running = False
 
+
+    mainCharacter.updateGravity()
+    mainCharacter.draw()
+
+    
     py.display.update()
 
 
