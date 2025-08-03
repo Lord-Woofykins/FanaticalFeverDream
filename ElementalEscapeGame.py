b65938675807928fb54f6f0a02e4e5a4e83d3316
Ethanzzz21 <ethan.zaoui.au@gmail.com>
2025-08-03 21:26:19 +1000

    Tweaking player movement, added deceleration

--- ElementalEscapeGame.py
+++ ElementalEscapeGame.py
@@ -9,7 +9,11 @@ GREEN = (0, 128, 0)
 WIDTH, HEIGHT = 1100, 800
 
 GRAVITY = 0.5
-GROUND_HEIGHT = 525
+GROUND_HEIGHT = 725
+
+ACCELERATION = 1
+DECELERATION = 2
+max_speed = 8
 
 nameOfGame = "Roguelike Shapeshifter TEST"
 
@@ -19,7 +23,7 @@ screen = py.display.get_surface()
 
 clock = py.time.Clock()
 
-startingPosition = (400, 300)
+startingPosition = (400, 600)
 
 class Player:
     def __init__(self, name, width=50, height=50):
@@ -35,7 +39,8 @@ class Player:
 
     def movePlayer(self, acceleration_x):
         self.velocity_x += acceleration_x
-        self.velocity_x = max(-10, min(self.velocity_x, 10))  # Limit horizontal speed by: max(absolute minimum, actual speed capped at maximum)-> restricted velocity
+        self.velocity_x = max(-max_speed, min(self.velocity_x, max_speed))  # Limit horizontal speed by: max(absolute minimum, actual speed capped at maximum)-> restricted velocity
+        print(self.velocity_x, end=' ', flush=True)
         x, y = self.position
         future_x = x + self.velocity_x
 
@@ -48,7 +53,10 @@ class Player:
         self.position = (future_x, y)
 
         if acceleration_x == 0:
-            self.velocity_x = 0  # Stop horizontal movement if no input
+            if self.velocity_x > 0:
+                self.velocity_x -= DECELERATION
+            elif self.velocity_x < 0:
+                self.velocity_x += DECELERATION
         
         
         
@@ -83,14 +91,15 @@ class Player:
 mainCharacter = Player("Player1")
 mainCharacter.draw()
 
-ground = py.Rect(0, 550, WIDTH, 50)
+ground = py.Rect(0, GROUND_HEIGHT+25, WIDTH, 50)
 rooms = {
     "Beginning": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 }
 
 platforms = [
-    py.Rect(0, 550, WIDTH, 50)
+    py.Rect(0, GROUND_HEIGHT+25, WIDTH, 50)
 ]
+
 
 
 class Room():
@@ -111,7 +120,7 @@ while running:
     clock.tick(60) # Limit the frame rate to 60 FPS
 
     # Draw the environment/room -- TEMPORARY
-    py.draw.rect(screen, (100, 100, 100), ground)
+    
 
 
     # Respond to player input
@@ -121,13 +130,16 @@ while running:
             running = False
     
     keys = py.key.get_pressed()
-    if keys[py.K_a]:
-        mainCharacter.movePlayer(-1)  # Move left
-    if keys[py.K_d]:
-        mainCharacter.movePlayer(1)
+    if keys[py.K_a] or keys[py.K_d]:
+        if keys[py.K_a]:
+            mainCharacter.movePlayer(-ACCELERATION)  # Move left
+        if keys[py.K_d]:
+            mainCharacter.movePlayer(ACCELERATION)
+    else:
+        mainCharacter.movePlayer(0)  # Stop horizontal movement if no input
 
     for platform in platforms:
-        pass
+        py.draw.rect(screen, (100, 100, 100), ground)
 
 
 
