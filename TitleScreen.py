f403fb9a6ecc943e4161fa677f0f650560150fae
Lord-Woofykins <ethan.zaoui@student.scots.college>
2025-09-21 10:41:38 +1000

    Restructuring title screen code for readability and beginning work on selector arrow

--- TitleScreen.py
+++ TitleScreen.py
@@ -4,12 +4,41 @@ from ColourPalettes import themeColourPalettes
 
 class TitleScreen:
     def __init__(self):
+        self.antialiasing = False
+
+        # Store a local reference to colours for easy access
         self.white = themeColourPalettes["CorePalette"]["white"]
         self.black = themeColourPalettes["CorePalette"]["black"]
 
-        self.font = py.font.Font("Jacquard12-Regular.ttf", 74)
-        self.titleText = self.font.render("Roguelike Shapeshifter", True, self.white)
-        self.startText = self.font.render("Press Enter to Start", True, self.white)
+        # Create fonts and reder text surfaces
+        self.titleFont = py.font.Font("Jacquard12-Regular.ttf", 96)
+        self.bodyFont = py.font.Font("Jacquard12-Regular.ttf", 64)
+        self.titleText = self.titleFont.render("Roguelike Shapeshifter", self.antialiasing, self.white)
+        self.startText = self.bodyFont.render("Press Enter to Select", self.antialiasing, self.white)
+        self.newGameText = self.bodyFont.render("New Game", self.antialiasing, self.white)
+        self.continueText = self.bodyFont.render("Continue", self.antialiasing, self.white)
+        self.selectArrow = self.bodyFont.render(">>", self.antialiasing, self.white)
+
+        # Allocate positions for text elements
+        windowWidth, windowHeight = py.display.get_window_size()
+        self.textRectCoords = {
+            "titleText": (windowWidth // 2, windowHeight // 5),
+            "startText": (windowWidth // 2, windowHeight // 3),
+            "newGameText": (windowWidth // 2, windowHeight // 1.8),
+            "continueText": (windowWidth // 2, int(windowHeight // 1.5)),
+            "selectArrow": (windowWidth // 2 - 200, int(windowHeight // 1.8))
+        }
+
+        # Map text elements to surfaces
+        self.textSurfaces = {
+            "titleText": self.titleText,
+            "startText": self.startText,
+            "newGameText": self.newGameText,
+            "continueText": self.continueText,
+            "selectArrow": self.selectArrow
+        }
+
+        self.selectedOption = 0 # 0 for new game, 1 for continue
 
         self.clock = py.time.Clock()
 
@@ -17,11 +46,12 @@ class TitleScreen:
         screen = py.display.get_surface()
         screen.fill(self.black)
 
-        windowWidth, windowHeight = py.display.get_window_size()
-        titleRect = self.titleText.get_rect(center=(windowWidth // 2, windowHeight // 3))
-        startRect = self.startText.get_rect(center=(windowWidth // 2, windowHeight * 2 // 3))
-        screen.blit(self.titleText, titleRect)
-        screen.blit(self.startText, startRect)
+        for element in self.textRectCoords:
+            elementX, elementY = self.textRectCoords[element]
+            surface = self.textSurfaces[element]
+            elementRect = surface.get_rect(center=(elementX, elementY))
+            screen.blit(surface, elementRect)
+        
         py.display.flip()
 
     def run(self):
